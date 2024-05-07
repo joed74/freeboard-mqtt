eca85743fbcb9a7bc0868b00a5b6c59cc9613e11
joed74 <jochen.dolze@schulergroup.com>
2024-05-07 10:28:08 +0200

    Small css change

--- js/freeboard.plugins.js
+++ js/freeboard.plugins.js
@@ -1267,7 +1267,7 @@
 
 	freeboard.addStyle('.indicator-light', "border-radius:50%;width:22px;height:22px;border:2px solid #3d3d3d;margin-top:5px;float:left;background-color:#222;margin-right:10px;");
 	freeboard.addStyle('.indicator-light.on', "background-color:#FFC773;box-shadow: 0px 0px 15px #FF9900;border-color:#FDF1DF;");
-	freeboard.addStyle('.indicator-text', "margin-top:10px;");
+	freeboard.addStyle('.indicator-text', "margin-top:8px;font-size:large;color:#d3d4d4");
     var indicatorWidget = function (settings) {
         var self = this;
         var titleElement = $('<h2 class="section-title"></h2>');
